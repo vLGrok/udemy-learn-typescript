8a05f967c72d6e35b03f488fc691658693eaa214
Robert Kerr <code@scanhelper.com>
2024-10-10 16:05:39 -0400

    variables

---
+++ src/variables.ts
@@ -0,0 +1,7 @@
+let movies = ["The Godfather", "The Dark Knight", "Pulp Fiction"];
+let foundMovie: string | undefined;
+for (let movie of movies) {
+    if (movie === "The Dark Knight") {
+        foundMovie = movie;
+    }
+}
